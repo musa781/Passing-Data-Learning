451e5fb733ed27bcac95e6b52b5c2966289cafd4
Muhammad Usama Sharif <112772227+musa781@users.noreply.github.com>
2025-05-11 15:32:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+
+
+});
+
+app.post("/submit", (req, res) => {
+  const numletters = req.body["fName"].length + req.body["lName"].length;
+  //console.log(numletters);
+  res.render("index.ejs", {letters : numletters});
+
+
+
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
